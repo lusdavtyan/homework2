e8a33dd27f92a609e6a2d4c6acce242f5e0ae7bf
lusdavtyan <lusdavtyan00@gmail.com>
2018-09-08 22:44:50 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,25 @@
+const a = 5;
+const b = 1;
+const b1 = '1';
+var d = b - a;
+var c = b + a;
+var f = 5.15;
+var bool = true;
+var num = 100;
+var str = 'this string';
+if(b1 === b){
+	alert('yes');
+}else{
+	alert('no');
+}
+if(b1 == b){
+	console.log('yes');
+}else{
+	console.log('no');
+}
+
+
+console.log(c*d);
+alert(a/b);
+console.log(c+" "+d);
+
